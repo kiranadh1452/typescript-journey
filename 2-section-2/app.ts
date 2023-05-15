48885de17a48e6e1cf134a5ca480f756c27f0fba
Kiran Adhikari <kiranadh1452@gmail.com>
2023-05-15 23:29:24 +0545

    Basic layout for the app complete with tsconfig file as well

---
+++ 2-section-2/app.ts
@@ -0,0 +1,37 @@
+// import the required packages
+import * as dotenv from "dotenv";
+import bodyParser from "body-parser";
+import express, { Express, Request, Response, NextFunction } from "express";
+
+// set up dotenv, to allow us to use environment variables
+dotenv.config({
+    path: "./src/config/config.env",
+});
+
+// creating and configuring the app
+const port = process.env.PORT || 8000;
+const app: Express = express();
+
+app.set("port", port);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+    return res.status(200).json({
+        success: true,
+        message: "Express app is working",
+    });
+});
+
+// Sample route
+app.use("*", (req: Request, res: Response, next: NextFunction) => {
+    return res.status(404).json({
+        success: false,
+        message: "API endpoint doesn't exist",
+    });
+});
+
+// starting the server
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
